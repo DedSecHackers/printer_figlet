6edf1a067a6c762da1c242a3204101036907a450
DedSecHackers <106261765+DedSecHackers@users.noreply.github.com>
2022-06-04 16:34:55 +0430

    Add files via upload

---
+++ printer.py
@@ -0,0 +1,31 @@
+import os
+
+os.system('pip install pyfiglet')
+os.system('pip install termcolor2')
+
+import pyfiglet
+from termcolor2 import colored
+
+os.system('clear')
+
+
+chanel = '@termux_scripte'
+nexus = 'printer tools'
+# c = pyfiglet.figlet_format(chanel)
+z = pyfiglet.figlet_format(nexus)
+p = colored(chanel, ('cyan'))
+pr = colored(z, ('yellow'))
+
+print(p)
+print(pr)
+
+
+massage = input('what would you like to print? : ') 
+# massage = colored(massage, ('blue'))
+colo = input('what color? : ')
+# colo = colored(colo, ('red'))
+ascii_art = pyfiglet.figlet_format(massage)
+as_art = colored(ascii_art, (colo))
+
+
+print(as_art)
